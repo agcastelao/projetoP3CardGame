6d7eeccc459bf04518467e2e1c9bd8969b0ca2e0
arthurbarbosaa <arthur.com50@gmail.com>
2023-09-28 23:47:03 +0000

    Implementacao da classe CartaUnique.

---
+++ Card Game/src/CartaUnique.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class CartaUnique extends Carta {
+    private String habilidadeExtra;
+
+    public CartaUnique(String nome, String imagem, String tipo, String raridade, int ataque, int defesa, int custo) {
+        super(nome, imagem, tipo, raridade, ataque + 1, defesa + 1, custo, "");
+        Random random = new Random();
+        setHabilidadeExtraAleatoria(random);
+    }
+
+    private void setHabilidadeExtraAleatoria(Random random) {
+        String[] habilidadesExtras = {"Veneno", "Escudo", "Teletransporte", "Invisibilidade", "Fogo", "Congelamento"};
+        int index = random.nextInt(habilidadesExtras.length);
+        habilidadeExtra = habilidadesExtras[index];
+    }
+
+    public String getHabilidadeExtra() {
+        return habilidadeExtra;
+    }
+
+    public void setHabilidadeExtra(String habilidadeExtra) {
+        this.habilidadeExtra = habilidadeExtra;
+    }
+
+}
