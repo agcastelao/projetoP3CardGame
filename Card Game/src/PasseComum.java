52b0d3a88889f2f9c3c85e32fc643f3a5d607c1d
arthurbarbosaa <arthur.com50@gmail.com>
2023-11-11 16:46:32 +0000

    alteracao no passe comum.

--- Card Game/src/PasseComum.java
+++ Card Game/src/PasseComum.java
@@ -1,7 +1,15 @@
 public class PasseComum extends Progresso{
 
-    public void subirNivelNoPasseComum() {
-        premiacaoAtual++;
+    public void subirNivelNoPasseComum() { //usar esse metodo ao vencer
+        if(this.premiacaoAtual == 60) {
+            return;
+        } else {
+            premiacaoAtual++;
+        }
+    }
+
+    public void entregarPremiacao() {
+        Inventario.adicionarCartasLiberadas(Loja.abrirBooster());
     }
 
     //usar abrirBoster() e adicionarCartasLiberadas(List<Carta> cartas)
--- Card Game/src/Progresso.java
+++ Card Game/src/Progresso.java
@@ -1,4 +1,12 @@
 public abstract class Progresso {
     String premiacoes[] = new String[60];
     int premiacaoAtual = 0;
+
+    public void entregarPremiacao() {
+
+    }
+
+    public int progresso() {
+        return this.premiacaoAtual;
+    }
 }
